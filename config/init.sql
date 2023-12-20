a468bec3f95fe624de75c76b25e86474e77d3765
Pedro Correa <pedro8correa@gmail.com>
2023-12-20 12:15:26 -0300

    :wrench: postgres configuration

---
+++ config/init.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS PESSOAS (
+    id VARCHAR(36),
+    apelido VARCHAR(32) CONSTRAINT ID_PK PRIMARY KEY,
+    nome VARCHAR(100),
+    nascimento CHAR(10),
+    stack VARCHAR(1024),
+    busca_trgm TEXT GENERATED ALWAYS AS (
+        LOWER(NOME || APELIDO || STACK)
+    ) STORED
+);
